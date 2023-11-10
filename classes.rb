9686759ebbc4f1aebfc6c1e64565d98c07e892a4
Arkadiusz Dziechciarz <arkadiusz.dziechciarz@gmail.com>
2023-11-10 16:13:46 +0100

    self example

--- classes.rb
+++ classes.rb
@@ -6,15 +6,19 @@ class Person
   def greet
     puts "Hello I am #{@name}"
   end
+
+  def what
+    puts "I am an instance method #{self}"
+  end
+
+  def self.what
+    puts "I am a class method #{self}"
+  end
+
 end
 
 person1 = Person.new("John")
-person1.greet
 
-class Person
-  def bye
-    puts "Bye Bye!"
-  end
-end
 
-person1.bye
+person1.what
+Person.what
